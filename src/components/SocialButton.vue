e247f553d9081a8d1a2af2d7b950f9f419ed9594
Beat Walti <BWalti@users.noreply.github.com>
2022-08-11 23:04:40 +0200

    fix: added safelist so that colors do not get tree shaken

--- src/components/SocialButton.vue
+++ src/components/SocialButton.vue
@@ -3,6 +3,8 @@
     class="flex border rounded-md justify-self-start m-4"
     :class="color"
     :href="link"
+    target="_blank"
+    rel="noopener"
   >
     <vue-feather :type="icon" class="m-4 my-4" stroke-width="2"></vue-feather>
     <p class="self-center mr-4 text-lg font-semibold">{{ name }}</p>
--- windi.config.ts
+++ windi.config.ts
@@ -1,6 +1,9 @@
-export default {
+import { defineConfig } from 'windicss/helpers'
+
+export default defineConfig({
   theme: {
     colors: {
+      // check: https://www.lockedownseo.com/social-media-colors/#instagram
       facebook: "#3b5998",
       twitter: "#1da1f2",
       instagram: "#c32aa3",
@@ -8,4 +11,13 @@ export default {
       contact: "#35465d",
     },
   },
-};
+
+  // see https://github.com/windicss/vite-plugin-windicss/blob/main/packages/plugin-utils/src/options.ts
+  safelist: [
+    "text-facebook",
+    "text-instagram",
+    "text-twitter",
+    "text-whatsapp",
+    "text-contact",
+  ],
+});
